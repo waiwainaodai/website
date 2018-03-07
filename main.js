828fcf4ee94b7dc6f2b27107807e5fe6912c6ebc
Chenyang Lou <louchenyang@outlook.com>
2018-03-07 12:30:02 +0800

    modify pic
    modify pic

--- main.js
+++ main.js
@@ -4,37 +4,38 @@ function random(){
     var number = parseInt(10*Math.random());
     if (number == 0)
     {
-        img.src = "./img/test1.jpg";
+        img.src = "./img/test1.png";
     }
     if (number == 1)
     {
-        img.src = "./img/test2.jpg";
+        img.src = "./img/test2.png";
     }
     if (number == 2)
     {
-        img.src = "./img/test3.jpg";
+        img.src = "./img/test5.png";
     }
     if (number == 3){
-        img.src = "./img/test4.jpg";
+        img.src = "./img/test6.png";
     }
     if (number == 4){
-        img.src = "./img/test5.jpg";
+        img.src = "./img/test7.png";
     }
     if (number == 5){
-        img.src = "./img/test6.jpg";
+        img.src = "./img/test8.png";
     }
-    if (number == 6){
-        img.src = "./img/test7.jpg";
-    }
-    if (number == 7){
-        img.src = "./img/test8.jpg";
-    }
-    if (number == 8){
-        img.src = "./img/test9.jpg";
-    }
-    if (number == 9){
-        img.src = "./img/test10.jpg";
-    }
+    
+    // if (number == 6){
+    //     img.src = "./img/test7.jpg";
+    // }
+    // if (number == 7){
+    //     img.src = "./img/test8.jpg";
+    // }
+    // if (number == 8){
+    //     img.src = "./img/test9.jpg";
+    // }
+    // if (number == 9){
+    //     img.src = "./img/test10.jpg";
+    // }
     
 
 }
