ac0227dbe4c592c3e85581de9e0bc1a938bd55d1
Chenyang Lou <louchenyang@outlook.com>
2018-03-08 13:08:16 +0800

    modify QRcode
    modify QRcode

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var img = document.getElementById("showimg");
 
 function random(){
-    var number = parseInt(10*Math.random())%6;
+    var number = parseInt(10*Math.random())%3;
     if (number == 0)
     {
         img.src = "./img/test1.png";
@@ -12,17 +12,17 @@ function random(){
     }
     if (number == 2)
     {
-        img.src = "./img/test5.png";
+        img.src = "./img/test3.png";
     }
-    if (number == 3){
-        img.src = "./img/test6.png";
-    }
-    if (number == 4){
-        img.src = "./img/test7.png";
-    }
-    if (number == 5){
-        img.src = "./img/test8.png";
-    }
+    // if (number == 3){
+    //     img.src = "./img/test6.png";
+    // }
+    // if (number == 4){
+    //     img.src = "./img/test7.png";
+    // }
+    // if (number == 5){
+    //     img.src = "./img/test8.png";
+    // }
     
     // if (number == 6){
     //     img.src = "./img/test7.jpg";
